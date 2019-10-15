152b547a22ee7484a6953169e320945b763bd686
Wolfgang Rönninger <wroennin@student.ethz.ch>
2019-10-15 17:35:37 +0200

    added memory compiler libs

--- fpga/scripts/prologue.tcl
+++ fpga/scripts/prologue.tcl
@@ -18,6 +18,7 @@ set project ariane
 
 create_project $project . -force -part $::env(XILINX_PART)
 set_property board_part $::env(XILINX_BOARD) [current_project]
+set_property XPM_LIBRARIES XPM_MEMORY [current_project]
 
 # set number of threads to 8 (maximum, unfortunately)
 set_param general.maxThreads 8
